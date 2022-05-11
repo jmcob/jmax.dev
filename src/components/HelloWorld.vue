b34a90d1c2a691a47e3b1efe8f447c69e3be247a
jmcob <77777490+jmcob@users.noreply.github.com>
2022-05-11 11:39:58 +0200

    helloWorld.vue update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,88 +11,21 @@
       Projet 2, une maquette HTML/CSS :
       <a href="http://reservia.jmax.dev/"> Reservia</a>
     </h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+    <h3>
+      Projet 3, un MVP mobile first avec animations CSS :
+      <a href="http://ohmyfood.jmax.dev/"> OhMyFood</a>
+    </h3>
+
+    <h3>
+      Projet 5, un site ecommerce :
+      <a href="http://kanap.jmax.dev/"> Kanap</a>
+    </h3>
+    <h3>
+      Projet 6, le backend simple d'une appli collaborative de cuisine :
+      <a href="https://www.jmax.dev/html/projets/piiquante.html">
+        description de Piiquante</a
+      >
+    </h3>
   </div>
 </template>
 
