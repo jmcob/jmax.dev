ecf67b32f54366095d4d69a9bacc9a1a65d93d20
jmcob <77777490+jmcob@users.noreply.github.com>
2022-05-11 13:13:53 +0200

    CV littéral

---
+++ src/components/CVItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <h3 @click="show = !show">{{ title }}</h3>
+  <p v-if="show">{{ content }}</p>
+</template>
+
+<script>
+export default {
+  name: "CVItem",
+  props: {
+    title: String,
+    content: String,
+  },
+  data() {
+    return {
+      show: false,
+    };
+  },
+};
+</script>
+
+<style>
+h3 {
+  cursor: pointer;
+}
+</style>
--- src/views/CVView.vue
+++ src/views/CVView.vue
@@ -1,18 +1,38 @@
 <template>
-  <h1>Développeur Web - CV</h1>
+  <h1>Développeur Web JS, front & back - CV</h1>
   <h2>Jean-Maxime Cobac, créatif et disponible</h2>
   <p>Récemment diplômé par OpenClassrooms, je cherche un premier emploi.</p>
-  <h3>Compétences</h3>
-  |
-  <h3>Etudes</h3>
-  |
-  <h3>Contact et informations</h3>
-  |
-  <h3>Soft Skills</h3>
-  |
-  <h3>Super Pouvoirs</h3>
-  |
-  <h3>Emplois précédents</h3>
+  <h3>Bonjour !</h3>
+  <p>
+    J'ai appris à coder il y a 20 ans l'<strong>HTML4</strong>, ces pages
+    aujourd'hui n'existent plus !
+    <br />(Et le Basic sur calculatrice scientifique)
+    <br />
+    En 2020, j'apprends la version 5 du markup avec le <strong>CSS</strong>.
+    <br />
+    En 2021, j'intègre la promotion
+    <strong>Web Developer d'OpenClassrooms</strong>.
+  </p>
+  <h3>
+    logiquement d'autres langages, <br />
+    frameworks <br />
+    et bases de données ont suivi...
+  </h3>
+  <p>
+    Le Sass, <br />
+    <strong>le JavaScript, Vue3, Express</strong>, <br />
+    NodeJS, <strong>MongoDB, MySQL</strong> (Sequelize), <br />
+    et même l'<strong>Anglais, lu écrit et parlé.</strong>
+  </p>
 </template>
 
+<script>
+// import CVItem from "@/components/CVItem.vue";
+// export default {
+//   name: "CVView",
+//   components: {
+//     CVItem,
+//   },
+// };
+</script>
 <style></style>
