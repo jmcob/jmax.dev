e5bfe17d2baca95d1a5c2ded810179ae7fe60e48
jmcob <77777490+jmcob@users.noreply.github.com>
2022-06-26 11:18:39 +0200

    contact maj 5

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="about">
     <h1>Coded by Jean-Maxime Cobac,</h1>
-    <h2>in May 2022, using vue-js 3.</h2>
-    <p>Version 3.3</p>
+    <h2>in May and June 2022, using vue-js 3.</h2>
+    <p>Version 3.4</p>
     <img alt="About logo" src="../assets/about.png" />
   </div>
 </template>
