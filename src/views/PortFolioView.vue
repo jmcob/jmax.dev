6b8a032003d66cf07626a49e901bcab05b9cb5ed
jmcob <77777490+jmcob@users.noreply.github.com>
2022-07-05 07:54:47 +0200

    portfolio revu en flexbox

--- src/views/PortFolioView.vue
+++ src/views/PortFolioView.vue
@@ -17,35 +17,60 @@
         <br />
         <h2>Projets OpenClassrooms</h2>
         <p>Voici quelques-uns des projets que j'ai réalisé :</p>
-        <h3>
-          <a href="http://reservia.jmax.dev/">Reservia</a>
-        </h3>
-        <p>Une maquette HTML/CSS à réaliser pour le projet 2.</p>
-        <h3>
-          <a href="http://ohmyfood.jmax.dev/"> OhMyFood</a>
-        </h3>
-        <p>Projet 3, un MVP mobile first avec animations CSS.</p>
-        <h3>
-          <a href="http://kanap.jmax.dev/"> Kanap</a>
-        </h3>
-        <p>Projet 5, un site e-commerce en JavaScript Vanilla</p>
-        <h3><router-link to="/piiquante">Piiquante</router-link></h3>
-        <p>
-          Projet 6, le backend simple d'une appli collaborative de cuisine. En
-          JavaScript node.js, express et MongoDB.
-        </p>
-        <h3>
-          <a href="https://youtu.be/ZzJ1LImNdZc"
-            >Démo de Groupomania (sur YouTube)</a
-          >
-        </h3>
-        <p>
-          Le projet 7 est un réseau social qui n'est pas en ligne. <br />
-          Une courte vidéo de présentation est disponible sur Youtube.
-          <br />
-          Ce projet a été réalisé avec express et Vue.js 3, donc le backend et
-          le frontend en JavaScript. Il utilise MySQL.
-        </p>
+        <div class="projects">
+          <div class="card">
+            <h3>
+              <a href="https://youtu.be/ZzJ1LImNdZc"
+                >Démo de Groupomania (sur YouTube)</a
+              >
+            </h3>
+            <p>
+              Le projet 7 est un <strong>réseau social</strong> dont la démo
+              vidéo est en ligne sur YouTube.
+              <br />
+              Ce projet a été réalisé avec express et <strong>Vue.js 3</strong>,
+              donc le backend et le frontend en JavaScript. Il utilise
+              <strong>MySQL (Sequelize)</strong>.
+            </p>
+          </div>
+          <div class="card">
+            <h3><router-link to="/piiquante">Piiquante</router-link></h3>
+            <p>
+              Projet 6, le <strong>backend</strong> simple d'une appli
+              collaborative de cuisine. <br />
+              <br />
+              En JavaScript
+              <strong>node.js, express et MongoDB.</strong>
+            </p>
+          </div>
+          <div class="card">
+            <h3>
+              <a href="http://reservia.jmax.dev/">Reservia</a>
+            </h3>
+            <p>
+              Une maquette <strong>HTML/CSS</strong> à réaliser pour le projet
+              2.
+            </p>
+          </div>
+          <div class="card">
+            <h3>
+              <a href="http://ohmyfood.jmax.dev/"> OhMyFood</a>
+            </h3>
+            <p>
+              Projet 3, un MVP mobile first avec
+              <strong>animations CSS</strong>.
+            </p>
+          </div>
+          <div class="card">
+            <h3>
+              <a href="http://kanap.jmax.dev/"> Kanap</a>
+            </h3>
+            <p>
+              Projet 5, un
+              <strong>site e-commerce en JavaScript Vanilla</strong>
+            </p>
+          </div>
+        </div>
       </div>
     </div>
     <br />
@@ -58,15 +83,23 @@
         </div>
         <br />
         <h2>Projets personnels</h2>
-        <h3><a href="https://play.jmax.dev/">JavaScript Playground</a></h3>
-        <p>
-          Trois petits projets d'entrainement qui m'ont permis d'apprendre ce
-          langage.
-        </p>
-        <h3><a href="https://todo.jmax.dev/">Task Tracker</a></h3>
-        <p>Un projet Vue3 dont j'ai réalisé le back-end.</p>
-        <h3><a href="https://www.jmxi.art/">jmxi.art</a></h3>
-        <p>Un projet artistique religieux personnel.</p>
+        <div class="projects">
+          <div class="card">
+            <h3><a href="https://play.jmax.dev/">JavaScript Playground</a></h3>
+            <p>
+              Trois petits projets d'entrainement qui m'ont permis d'apprendre
+              ce langage.
+            </p>
+          </div>
+          <div class="card">
+            <h3><a href="https://todo.jmax.dev/">Task Tracker</a></h3>
+            <p>Un projet Vue3 dont j'ai réalisé le back-end.</p>
+          </div>
+          <div class="card">
+            <h3><a href="https://www.jmxi.art/">jmxi.art</a></h3>
+            <p>Un projet artistique religieux personnel.</p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -98,5 +131,15 @@ h2 {
   /* img size is not the same (77 instead of 81) */
   width: 77px;
   height: 77px;
+}
+.projects {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  grid-gap: 5%;
+}
+.card {
+  width: 45%;
 }
 </style>
